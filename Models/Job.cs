3aaac756963c3c45f96a259834d9cb5804456fcc
d4niel <olayajuandaniel8@gmail.com>
2023-08-21 21:35:39 -0500

    dbcontext

---
+++ Models/Job.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyApp_Api.Models
+{
+    public partial class Job
+    {
+        public long IdTask { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool? Completed { get; set; }
+    }
+}
---
+++ Models/Taxes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyApp_Api.Models
+{
+    public partial class Taxes
+    {
+        public int Id { get; set; }
+        public double? Cost { get; set; }
+    }
+}
