222ad00ac7f5cbadf0ff83c75970aa105df4a905
d4niel <olayajuandaniel8@gmail.com>
2023-08-21 21:42:32 -0500

    secrets

--- Models/railwayContext.cs
+++ Models/railwayContext.cs
@@ -17,14 +17,13 @@ namespace MoneyApp_Api.Models
         }
 
         public virtual DbSet<Job> Jobs { get; set; } = null!;
-        public virtual DbSet<Taxes> Taxes { get; set; } = null!;
+        public virtual DbSet<Taxis> Taxes { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-//warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=containers-us-west-70.railway.app;port=7268;user=testuser;password=6qynfaqwscgjs;database=railway", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.1.0-mysql"));
+                optionsBuilder.UseMySql("name=ConnectionStrings:Db", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.1.0-mysql"));
             }
         }
 
@@ -43,7 +42,7 @@ namespace MoneyApp_Api.Models
                 entity.Property(e => e.Name).HasMaxLength(255);
             });
 
-            modelBuilder.Entity<Taxes>(entity =>
+            modelBuilder.Entity<Taxis>(entity =>
             {
                 entity.Property(e => e.Id)
                     .ValueGeneratedNever()
