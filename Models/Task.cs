d5fae4d953f2d4a88fbf085c0ff71719408f6e37
d4niel-olaya <olayajuandaniel8@gmail.com>
2023-08-24 07:25:07 -0500

    scv2

---
+++ Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MoneyApp_Api.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; } 
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public  bool Completed { get; set; }
+    }
+}
