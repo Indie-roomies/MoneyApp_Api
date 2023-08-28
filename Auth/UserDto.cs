2b054ee9768b8bd84a313bb447ab64ce9bdd3ad5
d4niel <olayajuandaniel8@gmail.com>
2023-08-27 21:31:29 -0500

    dto

---
+++ Auth/UserDto.cs
@@ -0,0 +1,12 @@
+
+
+namespace MoneyApp_Api.Dto;
+
+public class UserDto
+{
+    public int Id { get; set;} 
+
+    public string UserName { get; set;} = string.Empty;
+
+    public string Password  { get; set;} = string.Empty;
+}
